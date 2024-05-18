df7b05bd8e7de230d239e2adb4b2a0e88497c534
Malik Huzafah Khan <huzafahrajput10@gmail.com>
2024-05-19 02:30:57 +0500

    Updated the ui of delete modal

--- src/components/Bots/DeleteModal.jsx
+++ src/components/Bots/DeleteModal.jsx
@@ -9,22 +9,51 @@ import {
   ModalOverlay,
 } from "@chakra-ui/react";
 import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+import { IoCloseOutline } from "react-icons/io5";
 
 export default function DeleteModal({ isOpen, onClose, handleDelete }) {
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>
+      <ModalContent bg={"#414345"} color={"#ffffff"}>
+        <ModalHeader fontWeight={"450"}>
           Delete Bot
-          <ModalCloseButton />
+          <ModalCloseButton
+            borderRadius={"full"}
+            _hover={{
+              bg: "linear-gradient(to right, #4e54c8, #8f94fb)",
+              color: "white",
+            }}
+          />
         </ModalHeader>
         <ModalBody>Are you sure you want to delete this bot?</ModalBody>
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
+          <Button
+            colorScheme="blue"
+            mr={3}
+            onClick={onClose}
+            borderRadius={"full"}
+            bg={"linear-gradient(to right, #4568dc, #b06ab3)"}
+            color={"#ffffff"}
+            fontWeight={"400"}
+            _hover={{
+              bg: "linear-gradient(to right, #6a11cb, #2575fc)",
+            }}
+            leftIcon={<IoCloseOutline size={20} />}
+          >
             Close
           </Button>
-          <Button colorScheme="red" onClick={handleDelete}>
+          <Button
+            colorScheme="red"
+            onClick={handleDelete}
+            bg={"linear-gradient(to right, #F44336, #E57373)"}
+            _hover={{
+              bg: "linear-gradient(to right, #D32F2F, #EF5350)",
+            }}
+            borderRadius={"full"}
+            rightIcon={<AiOutlineDelete color="white" fontSize={"20px"} />}
+          >
             Delete
           </Button>
         </ModalFooter>
