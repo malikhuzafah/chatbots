7f1bacacf989810fc1ac5143091d2e2dd4a1ea93
Malik Huzafah Khan <huzafahrajput10@gmail.com>
2024-05-25 01:15:32 +0500

    Added delete loading and not found page

--- src/app/dashboard/bots/page.js
+++ src/app/dashboard/bots/page.js
@@ -1,13 +1,23 @@
 "use client";
 
 import BotCard from "@/components/Bots/BotCard";
+import CreateModal from "@/components/Bots/CreateModal";
 import DeleteModal from "@/components/Bots/DeleteModal";
 import DetailsModal from "@/components/Bots/DetailsModal";
 import EditModal from "@/components/Bots/EditModal";
 import { BASE_URL } from "@/constants/constants";
-import { Flex, useDisclosure, Spinner } from "@chakra-ui/react";
+import {
+  Flex,
+  useDisclosure,
+  Spinner,
+  Image,
+  useToast,
+  Button,
+  Text,
+} from "@chakra-ui/react";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { BsStars } from "react-icons/bs";
 
 const Bots = () => {
   const [bots, setBots] = useState([]);
@@ -17,6 +27,10 @@ const Bots = () => {
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
+  const [isCreate, setIsCreate] = useState(false);
+
+  const toast = useToast();
 
   const wordLimit = 30;
 
@@ -40,9 +54,10 @@ const Bots = () => {
     getBots();
   }, [refresh]);
 
-  const deleteBot = async (id) => {
+  const deleteBot = async (bot) => {
     try {
-      const response = await axios.delete(`${BASE_URL}api/bots/${id}`, {
+      setDeleteLoading(true);
+      const response = await axios.delete(`${BASE_URL}api/bots/${bot._id}`, {
         headers: {
           "x-auth-token": localStorage.getItem("token"),
         },
@@ -53,6 +68,7 @@ const Bots = () => {
     } catch (error) {
       console.log(error);
     }
+    setDeleteLoading(false);
   };
 
   const handleShowMore = (bot) => {
@@ -88,21 +104,59 @@ const Bots = () => {
         <Flex
           flexWrap={"wrap"}
           gap={5}
-          justify={"center"}
+          // justify={"center"}
           width={"100%"}
           mb={10}
           pb={10}
+          px={10}
         >
-          {bots.map((bot, index) => (
-            <BotCard
-              bot={bot}
-              setDeleteModalOpen={setDeleteModalOpen}
-              setEditModalOpen={setEditModalOpen}
-              setSelectedBot={setSelectedBot}
-              handleShowMore={handleShowMore}
-              key={index}
-            />
-          ))}
+          {bots.length > 0 ? (
+            bots.map((bot, index) => (
+              <BotCard
+                bot={bot}
+                setDeleteModalOpen={setDeleteModalOpen}
+                setEditModalOpen={setEditModalOpen}
+                setSelectedBot={setSelectedBot}
+                handleShowMore={handleShowMore}
+                key={index}
+              />
+            ))
+          ) : (
+            <Flex
+              width={"100%"}
+              height={"100%"}
+              justify={"center"}
+              align={"center"}
+            >
+              <Flex
+                h={300}
+                w={400}
+                bgColor={"#ffffff"}
+                borderRadius={10}
+                justify={"center"}
+                align={"center"}
+                flexDir={"column"}
+                gap={3}
+              >
+                <Image h={50} w={50} src="/images/no-bots.png" />
+                <Text>You have not yet created any bot!</Text>
+                <Button
+                  onClick={() => setIsCreate(true)}
+                  rightIcon={<BsStars />}
+                  borderRadius={"full"}
+                  bg={"linear-gradient(to right, #4568dc, #b06ab3)"}
+                  color={"#ffffff"}
+                  fontWeight={"400"}
+                  _hover={{
+                    bg: "linear-gradient(to right, #6a11cb, #2575fc)",
+                  }}
+                  transition={"background 0.3s ease"}
+                >
+                  Create Bot
+                </Button>
+              </Flex>
+            </Flex>
+          )}
         </Flex>
       )}
       <DetailsModal
@@ -122,7 +176,9 @@ const Bots = () => {
         handleDelete={() => {
           deleteBot(selectedBot);
         }}
+        isLoading={deleteLoading}
       />
+      <CreateModal isOpen={isCreate} onClose={() => setIsCreate(false)} />
     </Flex>
   );
 };
